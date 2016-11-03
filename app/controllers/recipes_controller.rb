0f98db2fe448cfd60b29130bf3a2c5fe078ce16d
Suhas <suhas-kumar@uiowa.edu>
2016-11-03 01:14:31 +0000

    created new and edit form, added partials, added update function

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -8,5 +8,51 @@ class RecipesController < ApplicationController
     @recipe = Recipe.find(params[:id]) 
     #binding.pry #making use of gem pry which was added
    end
+   
+   
+   def new 
+    
+    @recipe = Recipe.new
+    
+   end
+   
+   def create
+    
+    #binding.pry .. In console ...params ... this will tell what all we are getting
+    @recipe = Recipe.new(recipe_params) #match the signature and will take data to new create new recipe
+    @recipe.chef = Chef.find(5)
+    
+      if @recipe.save
+        flash[:success] = "Your recipe was create successfully"
+        redirect_to recipes_path
+      else
+     
+        render :new 
+      end
+    
+   end
+   
+   
+   def edit
+    @recipe = Recipe.find(params[:id])
+   end
+   
+   
+   def update
+     @recipe = Recipe.find(params[:id])
+      if @recipe.update(recipe_params)
+        flash[:success] = "Your recipe was updated successfully"
+        redirect_to recipes_path(@recipe)
+      else
+        render :edit
+      end
+   end
+   
+   
+   private 
+    
+    def  recipe_params 
+      params.require(:recipe).permit(:name, :summary, :description)
+    end
     
 end    
