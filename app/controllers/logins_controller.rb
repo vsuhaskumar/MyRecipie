95581c1a42fa75eb9fbbd4fa2a51d6728ba23eb3
Suhas <suhas-kumar@uiowa.edu>
2017-01-02 12:32:35 +0000

    test

--- app/controllers/logins_controller.rb
+++ app/controllers/logins_controller.rb
@@ -19,7 +19,7 @@ class LoginsController < ApplicationController
    def destroy 
      session[:chef_id] = nil
      flash[:success] = "You have logged out"
-     redirect_to root_path
+     redirect_to roots_path
    end
     
 end
