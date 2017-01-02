a679e42ef8eb5e85ce1132a3ef293c4aaff7e589
Suhas <suhas-kumar@uiowa.edu>
2017-01-02 04:06:49 +0000

    Added routes, created model, controllers and view for style and ingredients

--- config/routes.rb
+++ config/routes.rb
@@ -28,6 +28,9 @@ Rails.application.routes.draw do
   get '/login', to: "logins#new"
   post '/login', to: "logins#create"
   get '/logout', to: "logins#destroy"
-        
+  
+  
+  resources :styles, only: [:new, :create, :show]
+  resources :ingredients, only: [:new, :create, :show]        
     
 end
